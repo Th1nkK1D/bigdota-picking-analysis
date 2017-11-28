7b4fb111b587d2abf523bf0e1fb8548d4f586552
Withee Poositasai <witheep@gmail.com>
2017-11-28 20:19:22 +0700

    Replace get json with axios

--- getMatchesData.js
+++ getMatchesData.js
@@ -1,10 +1,32 @@
-const getJSON = require('get-json')
+const axios = require('axios')
 
+async function fetchMatches(num) {
+    let result = []
+    let lastMatchId
 
-getJSON('https://api.opendota.com/api/publicMatches', function(error, response){
-    if(error) {
-        console.log(error)
-    } else {    
-        console.log(response.length)
+    for(let i = 0; i < num; i++) {
+        let res
+
+        try {
+            res = await axios.get('https://api.opendota.com/api/publicMatches',{
+                params: {
+                    'less_than_match_id': lastMatchId
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+        // console.log(res.data)
+        
+        result = result.concat(res.data)
+        lastMatchId = res.data[res.data.length-1].match_id
+        console.log(lastMatchId)
     }
+    
+    return result;
+}
+
+fetchMatches(2).then(res => {
+    console.log(res.length)
 })
