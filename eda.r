f2b319717ff5fd92b6f09d269980a0f0be88b0fe
Withee Poositasai <witheep@gmail.com>
2017-11-29 19:05:45 +0700

    Re-organizing data frame

--- eda.r
+++ eda.r
@@ -8,12 +8,11 @@ rawdata <- fromJSON(txt="matches.json")
 data <- rawdata[,c('match_id','radiant_win','radiant_team','dire_team')]
 
 # Group winner, loser
-data <- mutate(data,
-               winner = case_when(radiant_win ~ radiant_team, !radiant_win ~ dire_team), 
-               loser = case_when(!radiant_win ~ radiant_team, radiant_win ~ dire_team))
+data %>% mutate(winner = case_when(radiant_win ~ radiant_team, !radiant_win ~ dire_team), 
+               loser = case_when(!radiant_win ~ radiant_team, radiant_win ~ dire_team)) -> data
 
-# Split heroes in to each column
-data <- separate(data, winner, c('w1', 'w2','w3','w4','w5'), ',')
-data <- separate(data, loser, c('l1', 'l2','l3','l4','l5'), ',')
+# Split heroes string
+data %>% separate(loser, c('l1', 'l2','l3','l4','l5'), ',') -> data
+data %>% transform(winner = strsplit(winner, ",")) %>% unnest(winner) -> data 
 
-data <- data[,c('match_id','w1', 'w2','w3','w4','w5','l1', 'l2','l3','l4','l5')]
+data <- data[,c('match_id','l1', 'l2','l3','l4','l5','winner')]
